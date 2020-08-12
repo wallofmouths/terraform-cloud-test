3bd84fb677e97ee02cd0edab06b95ac58015daaa
Michael McCarthy <michael.mccarthy@gamesys.co.uk>
2020-08-12 22:19:23 +0100

    Initial commit

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
 provider "github" {
-  token        = "${var.github_token}"
-  organization = "${var.github_organization}"
 }
 
 resource "github_repository" "example" {
   name        = "example"
   description = "My awesome codebase"
-
   private = true
-
 }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "github_organization" {
+    type = string
+    default = "mikeycmccarthy"
+}
+
+variable "github_token" {
+    type = string
+}
